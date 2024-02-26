8fc61cb95aa7a8c8648b5b7404e338a9727fbd91
sammy ocharo <obanyisammy22@gmail.com>
2024-02-26 22:41:45 +0300

    this is the initial commit of the books microservice

---
+++ src/main/java/com/samdev/books/microservice/BooksMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.samdev.books.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samdev/books/microservice/BooksMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samdev.books.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
